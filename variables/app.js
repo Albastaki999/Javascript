1562a80af61445a8345897907748b80a265311fd
Rashid Mahmood <rashidmahmood06056@gmail.com>
2026-08-19 18:01:49 +0530

    first commit

---
+++ variables/app.js
@@ -0,0 +1,49 @@
+// Declaration: Letting the compiler know that there is a variable called x
+// Let - keyword
+
+// 1. Declare
+// let x;
+
+// 2. Assignment
+// x = 100
+
+// Declare + assign
+let age = 100;
+
+// Name of variable -> identifier
+
+// console.log(age);
+
+// Identifier rules:
+// 1. Never start with a number, number can come in middle or end
+// 2. can start with alphabets, $ or _
+// 3. Never use keywords
+// 4. Never put spaces
+let firstName = "Rashid";
+firstName = "Abcd";
+firstName = "sgahs";
+firstName = 10;
+firstName = 100;
+
+// Constants -> used for storing things that do not change
+const a = 100;
+const b = a;
+
+let x;
+let y;
+let random = "hello";
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+x = y = z = random;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+let num1 = 10;
+let num2 = 20;
+const sum = num1 + num2;
+console.log(sum);
