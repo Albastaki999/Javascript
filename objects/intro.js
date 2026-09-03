b9cffb64479e593535af9f480fe42dc8daf9ab77
Rashid Mahmood <rashidmahmood06056@gmail.com>
2026-09-03 18:13:49 +0530

    objects intro

---
+++ objects/intro.js
@@ -0,0 +1,56 @@
+
+const student = {
+    firstName: "Rashid",
+    lastName: "Mahmood",
+    age: 23,
+    phone: "8271628171",
+    courses: ["java", "MERN", "Cybersecurity"],
+    paidFees: true,
+    address: {
+        line1: "abcd",
+        line2: "ajhs",
+        state: "TS",
+        city: "Hyderabad",
+        zip: "500019"
+    }
+}
+
+// console.log("firstName" in student);
+
+// console.log(student.isEligibleToVote);
+// student.isEligibleToVote = true
+// console.log(student);
+
+
+// student.firstName = "ABC"
+// delete student.lastName
+
+// console.log(student);
+// console.log(student.address);
+
+
+// console.log(student);
+// console.log(typeof student);
+// console.log(student.firstName); // -> dot notation
+// console.log(student["firstName"]);
+
+// console.log(student.firstName + " " + student.lastName);
+
+
+// console.log(student.lastName);
+// student.courses[0] = "AIML"
+// console.log(student.courses[0]);
+
+// if (student.paidFees) {
+//     console.log("Paid");
+// }
+// else {
+//     console.log("Not paid");
+    
+// }
+
+// let student2 = {...student}; // shallow copy
+// let student2 = structuredClone(student)
+// student2.address.line1 = "xyz"
+// console.log(student);
+
